dc776c2fa38cda60f6a3e067e6d38b49811b3f3e
jeffng5 <jeffreykng@hotmail.com>
2024-05-08 23:55:18 -0400

    added gallery/about view toggle

---
+++ about.js
@@ -0,0 +1,34 @@
+
+
+
+function showAbout() {
+    const showAboutButton = document.getElementsByTagName('button')
+    console.log(showAboutButton)
+    const h1 = document.getElementsByTagName('h1')
+    showAboutButton[0].addEventListener('click', function(e) {e.preventDefault(); 
+        toggleText() 
+  
+    })
+};
+
+// function addAbout() {    
+//     const h1 = document.createElement('h1');
+//     document.body.appendChild(h1)
+//     h1.classList.add('about')
+//     }
+   
+function toggleText() {
+    const about1 = document.getElementsByClassName('about')
+    console.log(about1)
+    Array.from(about1).forEach((x) => {
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      })
+
+}
+
+
+showAbout()
--- gallery.js
+++ gallery.js
@@ -1,24 +1,52 @@
 const galleryArr = [
     'backyard.png', 'bedroom.png', 'bedroom1.png', 'diningroom.png',
     'foyer.png', 'gameroom.png', 'gameroom2.png', 'kitchen.png', 'kitchen-coffee.png', 'livingroom.png', 'patio.png', 'patio1.png'
-] 
-
-
-
-function createGallery() {
-    const h1 = document.createElement("h1");
-    h1.innerHTML += 'GALLERY'
-    h1.classList.add("gallery")
-
-    for (let ele in galleryArr) {
-    const img = document.createElement('img');
-    img.setAttribute('src', "./images/" + ele)
-    img.classList.add("enlarged") 
-    img.innerHTML += 'onclick="toggleEnlarged(this)"'
-
-    }
-  }
+];
 
 function toggleEnlarged(img) {
     img.classList.toggle("enlarged");
 }
+
+function createGallery() {
+
+    const buildGallery = document.getElementsByTagName('button');
+    console.log(buildGallery)
+    let arr = [...buildGallery];
+    galleryArr.forEach(ele => {mountPic(ele)})
+    arr[2].addEventListener('click', function(e) {e.preventDefault();
+    toggleGallery()})};
+
+
+function mountPic(ele) {
+    const img = document.createElement('img');
+    let h2 = document.getElementsByTagName('h2') 
+    h2[0].appendChild(img)
+    img.setAttribute('src', '/images/' + ele)
+    img.innerHTML += ` onclick="toggleEnlarged(img)"`
+    img.classList.add("enlarged") 
+    img.addEventListener('click', function(e) {e.preventDefault();
+    toggleEnlarged(img)})
+}
+
+
+function creatingGallery() {
+    galleryArr.forEach(ele => {mountPic(ele)
+}) }
+
+function toggleGallery() {
+    const about1 = document.getElementsByClassName('gallery')
+    console.log(about1)
+    Array.from(about1).forEach((x) => {
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      })
+
+}
+
+
+  
+
+createGallery()
