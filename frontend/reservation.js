aa40ffc0eac3205ff85ab75533f4de1f80b43ec1
jeffng5 <jeffreykng@hotmail.com>
2024-05-17 11:23:09 -0400

    fixed calendar

--- frontend/reservation.js
+++ frontend/reservation.js
@@ -4,7 +4,7 @@ let date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth();
  
-const day = document.querySelector(".calendar-weekdays");
+const day = document.querySelector(".calendar-dates");
  
 const currdate = document
     .querySelector(".calendar-current-date");
