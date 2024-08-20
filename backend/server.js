719ec4c35b8b52d35f7c51554ddc020b4a438e53
jeffng5 <jeffrey.ng51213@outlook.com>
2024-08-19 20:43:41 -0400

    changes to API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 "use strict";
-// This is your test secret API key.
-const stripe = require('stripe')('sk_test_51OlENfChg3YLxEh2jsQiHZLM36KeBDQOGhXFvFUsjsObXCoT7EjrbYTOTqvfzvNzxe7fmN9gKVQTvj1SGGCf42ck00KK4U7MsM');
+
 const express = require('express');
 const app = require('./app')
 app.use(express.static('public'));
